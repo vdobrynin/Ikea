3f0deebedb8fb638545d82d5280bfca35dbb6a36
vdobrynin <vdobrynin@sbcglobal.net>
2022-05-07 03:15:35 -0700

    Initial commit

---
+++ src/test/java/pages/IkeaHomePage.java
@@ -0,0 +1,8 @@
+package pages;
+
+public class Ikea extends Page {
+
+    public Ikea() {
+        setUrl("https://www.ikea.com/us/en");
+    }
+}
---
+++ src/test/java/support/Hooks.java
@@ -0,0 +1,31 @@
+package support;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import java.util.concurrent.TimeUnit;
+import static support.TestContest.getDriver;
+
+public class Hooks {
+
+    @Before(order = 0)
+    public void scenarioStart() {
+        TestContest.initialize();
+        getDriver().manage().deleteAllCookies();
+        getDriver().manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+        getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @After(order = 0)
+    public void scenarioEnd(Scenario scenario) {
+        if (scenario.isFailed()) {
+            TakesScreenshot screenshotTaker = (TakesScreenshot) getDriver();
+            byte[] screenshot = screenshotTaker.getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot, "image/png");
+        }
+        TestContest.teardown();
+    }
+}
+
