a85dffbd17eb58e21caf822ea415cd3b4ccacbd4
vdobrynin <vdobrynin@sbcglobal.net>
2022-05-09 13:48:35 -0700

    cleanUp code

--- src/test/java/pages/IkeaHomePage.java
+++ src/test/java/pages/IkeaHomePage.java
@@ -1,17 +1,12 @@
 package pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import static support.TestContest.getDriver;
-import static support.TestContest.getExecutor;
 
 public class IkeaHomePage extends Page {
 
--- src/test/java/pages/IkeaProductsPage.java
+++ src/test/java/pages/IkeaProductsPage.java
@@ -1,11 +1,9 @@
 package pages;
 
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 
-import static support.TestContest.getDriver;
 import static support.TestContest.getExecutor;
 
 public class IkeaProductsPage extends Page {
