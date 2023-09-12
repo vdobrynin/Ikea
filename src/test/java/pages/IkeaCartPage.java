d94122df9882e9ac9404f7a1884dee90eed33ce3
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2023-09-11 19:40:25 -0700

    fix some

--- src/test/java/pages/IkeaCartPage.java
+++ src/test/java/pages/IkeaCartPage.java
@@ -33,8 +33,8 @@ public class IkeaCartPage extends Page {
     private WebElement applyButton;
 
     public void clickAndEnterDiscountCode(String text) {
-        getExecutor()
-            .executeScript("window.scrollBy(0,-200)");;
+//        getExecutor()
+//            .executeScript("window.scrollBy(0,-100)");
         getWait()
             .until(ExpectedConditions.visibilityOf(discountHeader));
         discountHeader
