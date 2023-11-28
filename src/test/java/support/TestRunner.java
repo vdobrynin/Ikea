1ce14c0463be81d67111c97403cc8ccf0184d4c8
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2023-11-27 21:24:33 -0800

    update pom & fix

--- src/test/java/support/TestRunner.java
+++ src/test/java/support/TestRunner.java
@@ -1,20 +1,19 @@
 package support;
 
 import io.cucumber.junit.Cucumber;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    plugin = {"pretty", "html:target/cucumber", "json:target/cucumber/report.json"},
-    features = {"src/test/resources/features"},
+    plugin = {"pretty", "html:target/cucumber/report.html", "json:target/cucumber/report.json"},
+    features = "src/test/resources/features",
     glue = {"definitions", "support"},
-    tags = "@predefined" // same as VM option -Dcucumber.options="--tags @predefined"
+    tags = "@ikea_obj" // same as VM option -Dcucumber.options="--tags @predefined"
 )
-public class TestRunner extends AbstractTestNGCucumberTests {
+public class TestRunner {
 
   @BeforeClass
   public static void setup() {
